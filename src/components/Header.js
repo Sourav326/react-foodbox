945192ede70888aca5f39ec30554c08ce9c08f91
Sourav Chauhan <souravchauhan1964@gmail.com>
2023-10-13 00:24:37 +0530

    change cart icon

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,8 @@ import { useSelector } from "react-redux";
 import logo from "../logo.png";
 import "./css/Header.css";
 import { NavLink,Link } from "react-router-dom";
+import LocalMallIcon from '@mui/icons-material/LocalMall';
+import FavoriteIcon from '@mui/icons-material/Favorite';
 const Header = () => {
   function navbarToggle(){
     const element = document.getElementById("toggleNavbar");
@@ -83,21 +85,8 @@ const Header = () => {
                   </div>
                   <div className="pr-6 lg:pr-0 ml-4 flow-root lg:ml-6 lg:block">
                     <NavLink to="/cart" className="group -m-2 flex items-center p-2">
-                      <svg
-                        className="h-6 w-6 flex-shrink-0 text-gray-700 hover:text-lime-600 dark:text-white hover:text-lime-600"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        strokeWidth="1.5"
-                        stroke="currentColor"
-                        aria-hidden="true"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"
-                        />
-                      </svg>
-                      <span className="ml-2 text-sm font-medium text-gray-700 dark:text-white hover:text-lime-600">
+                      <LocalMallIcon className="text-gray-700 dark:text-white hover:text-lime-600 group" />
+                      <span className="ml-1 text-sm font-medium text-gray-700 dark:text-white group-hover:text-lime-600">
                         {cartItems.length}
                       </span>
                     </NavLink>
